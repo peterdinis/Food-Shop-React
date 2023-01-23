ea44f9c70def64db5b0e0dfc5d0112292a7a7715
Peterdinis <peterdinis0@pobox.sk>
2023-01-23 21:31:18 +0100

    Feat: Design UI

--- src/components/hero/Banner.tsx
+++ src/components/hero/Banner.tsx
@@ -1,9 +1,23 @@
-const Banner: React.FC = () => {
-    return (
-        <>
-        gggggggggg
-        </>
-    )
-}
+import Slider from "react-slick";
 
-export default Banner
+const Banner: React.FC = () => {
+  /* const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  }; */
+
+  return (
+    <div className="carousel relative container mx-auto" style={{
+        maxWidth: "1600px"
+    }}>
+        <div className="mt-8 carousel-inner relative overflow-hidden w-full">
+            FOO
+        </div>
+    </div>
+  )
+};
+
+export default Banner;
