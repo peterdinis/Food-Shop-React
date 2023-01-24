3b11771e3297646ff7b9dbeb815d3de31dac4ee7
Peterdinis <peterdinis0@pobox.sk>
2023-01-24 23:12:39 +0100

    fix: Fixing deploy issue

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import AuthContextProvider from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
