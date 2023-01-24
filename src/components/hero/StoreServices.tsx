102d47417233c640a93fa450ac45396afdc31e01
Peterdinis <peterdinis0@pobox.sk>
2023-01-24 17:08:11 +0100

    feat: Updating font and homepage code

--- src/components/hero/StoreServices.tsx
+++ src/components/hero/StoreServices.tsx
@@ -2,14 +2,13 @@ const StoreServices: React.FC = () => {
   return (
     <section className="bg-white py-8">
       <div className="container mx-auto flex items-center flex-wrap pt-4 pb-12">
-        <nav id="store" className="w-full z-30 top-0 px-6 py-1">
+        <nav className="w-full z-30 top-0 px-6 py-1">
           <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-2 py-3">
-            <a
-              className="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl "
-              href="#"
+            <span
+              className="tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl "
             >
               Our Top Products
-            </a>
+            </span>
           </div>
         </nav>
 
--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,8 +1,16 @@
 const Footer: React.FC = () => {
     return (
-        <>
-        fffff
-        </>
+        <section className="bg-white mt-3">
+        <div className="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
+          <nav className="flex flex-wrap justify-center -mx-5 -my-2">
+            <div className="px-5 py-2">
+              <span className="text-3xl leading-6 text-gray-800 hover:text-gray-900">
+                &copy;React FoodShop 2023
+              </span>
+            </div>
+          </nav>
+        </div>
+      </section>
     )
 }
 
