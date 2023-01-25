6111d9297ccefd6e0d7c4b5f25fbdc2e57e4713d
Peterdinis <peterdinis0@pobox.sk>
2023-01-25 18:10:17 +0100

    feat: Create new page notallowed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/shared/Navbar";
-import { LandingPage, NotFoundPage } from "./pages";
+import { LandingPage, NotAllowedPage, NotFoundPage } from "./pages";
 import { ProductInfo, ProductsPage } from "./pages/products";
 import { CardPage } from "./pages/card";
 import { RegisterPage, LoginPage, ProfilePage, ForgotPasswordPage, ResetPasswordPage } from "./pages/auth";
@@ -19,6 +19,7 @@ function App() {
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/forgot/password" element={<ForgotPasswordPage />} />
         <Route path="/reset/password" element={<ResetPasswordPage />} />
+        <Route path="/notallowed" element={<NotAllowedPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
