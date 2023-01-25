e95d8cfaa4fd5e53310590bbd8e58fa207a067cc
Peterdinis <peterdinis0@pobox.sk>
2023-01-25 16:16:19 +0100

    feat: Adding helper fallback component

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import AuthContextProvider from './context/AuthContext'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import AuthContextProvider from "./context/AuthContext";
+import { FallBackLoader } from "./components/shared";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthContextProvider>
-        <App />
-      </AuthContextProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+    <React.Suspense fallback={<FallBackLoader />}>
+      <BrowserRouter>
+        <AuthContextProvider>
+          <App />
+        </AuthContextProvider>
+      </BrowserRouter>
+    </React.Suspense>
+  </React.StrictMode>
+);
