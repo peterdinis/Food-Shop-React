425b22a7f6e745c261afa79ea691fb618ceef27c
Peterdinis <peterdinis0@pobox.sk>
2023-01-25 16:56:55 +0100

    FOO

---
+++ src/__tests__/auth/auth.test.tsx
@@ -0,0 +1,8 @@
+import { test, beforeAll, describe } from "vitest";
+import {app} from "../../firebase/init";
+
+describe("Firebase Auth testing", () => {
+    test("Trying to register to app", async()=> {
+        return;
+    })
+})
