6e2e117d843c6698ca0956e318f5357fdb8b07b6
Peterdinis <peterdinis0@pobox.sk>
2023-01-26 21:12:06 +0100

    feat: Reset Password form

--- src/components/auth/ResetPasswordForm.tsx
+++ src/components/auth/ResetPasswordForm.tsx
@@ -1,14 +1,91 @@
 import { Header } from "../shared";
 import useSearchQuery from "../../hooks/useSearchQuery";
+import { useNavigate } from "react-router";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+type FormData = {
+  password: string;
+};
 
 const ResetPasswordForm: React.FC = () => {
-    const query = useSearchQuery();
-    return (
-        <>
-         <Header text="Reset Password" />
+  const query = useSearchQuery();
+  const navigate = useNavigate();
+  const [password, setPassword] = useState("");
+  const {
+    handleSubmit,
+    formState: { errors },
+    trigger,
+    register,
+  } = useForm<FormData>();
 
-        </>
-    )
-}
+  return (
+    <>
+      <Header text="Reset Password" />
+      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+        <form>
+          <div className="mb-4">
+            <div className="mb-2">
+              <label
+                className="block text-grey-darker text-sm font-bold mb-2"
+                htmlFor="username"
+              >
+                Password
+              </label>
+              <input
+                className="emailInput shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                id="password"
+                type="password"
+                placeholder="**********************************"
+                autoComplete="off"
+                required={true}
+                {...register("password", {
+                  required: "You must specify a password",
+                  pattern: {
+                    value:
+                      "^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[d]){1,})(?=(.*[W]){    1,})(?!.*s).{8,}$" as any,
+                    message:
+                      "Password should contain at least one number and one    special character",
+                  },
+                  minLength: {
+                    value: 8,
+                    message: "Password must be more than 8 characters",
+                  },
+                  maxLength: {
+                    value: 20,
+                    message: "Password must be less than 20 characters",
+                  },
+                })}
+                onKeyUp={() => {
+                  trigger("password");
+                }}
+              />
+
+              <p className="text-red-800">
+                {errors.password && errors.password.message}
+              </p>
+            </div>
+          </div>
+          <div>
+            <button
+              className="mt-4 bg-blue-100 p-2 rounded-lg font-bold"
+              type="submit"
+            >
+              Register
+            </button>
+            <div>
+              <a
+                className="mt-4 inline-block align-baseline font-bold text-2xl text-blue hover:text-blue-darker"
+                href="/login"
+              >
+                Login
+              </a>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
 
 export default ResetPasswordForm;
