5727a0144f0a26f75d4e3bbd0e0f719f189aca65
Peterdinis <peterdinis0@pobox.sk>
2023-01-26 09:45:55 +0100

    feat: Updating structure

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -3,3 +3,4 @@ import React from 'react';
 export const Navbar = React.lazy(() => import("./Navbar"));
 export const Footer = React.lazy(() => import("./Footer"));
 export const FallBackLoader = React.lazy(() => import("./FallbackLoader"));
+export const Header = React.lazy(() => import("./Header"));
