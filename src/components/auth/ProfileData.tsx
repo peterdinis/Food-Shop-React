f8c6060090cafe5e958e82f446f65e726e9f105e
Peterdinis <peterdinis0@pobox.sk>
2023-01-27 17:50:02 +0100

    feat: Updating profile card

--- src/components/auth/ProfileData.tsx
+++ src/components/auth/ProfileData.tsx
@@ -18,7 +18,7 @@ const ProfileData: React.FC = () => {
       <div className="bg-white relative shadow rounded-lg w-5/6 md:w-5/6  lg:w-4/6 xl:w-3/6 mx-auto">
         <div className="flex justify-center">
           <img
-            src="https://avatars0.githubusercontent.com/u/35900628?v=4"
+            src="https://picsum.photos/200/300"
             alt=""
             className="rounded-full mx-auto absolute -top-20 w-32 h-32 shadow-md border-4 border-white transition duration-200 transform hover:scale-110"
           />
@@ -26,11 +26,38 @@ const ProfileData: React.FC = () => {
 
         <div className="mt-10">
           <h1 className="font-bold text-center text-3xl text-gray-900">
-            Pantazi Software
+          {currentUser?.email}
           </h1>
           <p className="text-center text-sm text-gray-400 font-medium">
-            UI Components Factory
+          <button className="mt-10 text-xl text-black font-bold" onClick={handleLogout}>Logout</button>
           </p>
+          <h4 className="mt-6 ml-4">My Last Orders</h4>
+          <div className="flex justify-between items-center my-5 px-6">
+            <a
+              href=""
+              className="text-gray-500 hover:text-gray-900 hover:bg-gray-100 rounded transition duration-150 ease-in font-medium text-sm text-center w-full py-3"
+            >
+              Facebook
+            </a>
+            <a
+              href=""
+              className="text-gray-500 hover:text-gray-900 hover:bg-gray-100 rounded transition duration-150 ease-in font-medium text-sm text-center w-full py-3"
+            >
+              Twitter
+            </a>
+            <a
+              href=""
+              className="text-gray-500 hover:text-gray-900 hover:bg-gray-100 rounded transition duration-150 ease-in font-medium text-sm text-center w-full py-3"
+            >
+              Instagram
+            </a>
+            <a
+              href=""
+              className="text-gray-500 hover:text-gray-900 hover:bg-gray-100 rounded transition duration-150 ease-in font-medium text-sm text-center w-full py-3"
+            >
+              Email
+            </a>
+          </div>
         </div>
       </div>
     </div>
