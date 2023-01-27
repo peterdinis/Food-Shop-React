317c4e4068a2ad9625f795cccded31c03ab8da3b
Peterdinis <peterdinis0@pobox.sk>
2023-01-27 12:56:41 +0100

    FOO

--- src/components/hero/Banner.tsx
+++ src/components/hero/Banner.tsx
@@ -1,16 +1,16 @@
 const Banner: React.FC = () => {
   return (
     <div className="w-full h-screen bg-opacity-50 bg-black flex justify-center items-center">
-        <div className="mx-4 text-center text-white mb-36">
-          <h1 className="font-bold text-8xl mb-4">FoodReShop</h1>
-          <h2 className="font-bold text-2xl mb-12">
-            Quality food for good prices
-          </h2>
-          <button className="bg-blue-100 p-2 rounded-lg text-xl font-bold text-black">
-            <a href="/products">Our Quality food</a>
-          </button>
-        </div>
+      <div className="mx-4 text-center text-white mb-36">
+        <h1 className="font-bold text-8xl mb-4">FoodReShop</h1>
+        <h2 className="font-bold text-2xl mb-12">
+          Quality food for good prices
+        </h2>
+        <button className="bg-blue-100 p-2 rounded-lg text-xl font-bold text-black">
+          <a href="/products">Our Quality food</a>
+        </button>
       </div>
+    </div>
   );
 };
 
