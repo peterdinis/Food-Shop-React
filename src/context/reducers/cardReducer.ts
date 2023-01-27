2c41d3d8ff8a1b8f3845810c1f5c6aab16a974c0
Peterdinis <peterdinis0@pobox.sk>
2023-01-28 00:21:39 +0100

    feat: New helper fn

--- src/context/reducers/cardReducer.ts
+++ src/context/reducers/cardReducer.ts
@@ -1,7 +1,15 @@
 import { ADD_TO_BASKET, REMOVE_ALL_ITEMS_FROM_BASKET, REMOVE_FROM_BASKET } from "../../typings/constants";
+import { IProduct } from "../../typings/productTypings";
 import { BasicAction, InitialStateI } from "../../typings/reduxTypings";
 
+
+export const getBasketTotal = (basket: any[]) => {
+  basket?.reduce((amount: number, item: IProduct) =>item.price + amount, 0);
+}
+
+
 const stateReducer = (state: InitialStateI, action: BasicAction): InitialStateI => {
+
     console.log(action);
     switch (action.type) {
       case ADD_TO_BASKET:
