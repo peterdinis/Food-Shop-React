4c40c5b5350830c1553add5bcad92e6ca3eff00e
Peterdinis <peterdinis0@pobox.sk>
2023-01-28 00:28:44 +0100

    fix: FIxing small issue in cardReducer

--- src/context/reducers/cardReducer.ts
+++ src/context/reducers/cardReducer.ts
@@ -20,10 +20,18 @@ const stateReducer = (state: InitialStateI, action: BasicAction): InitialStateI 
         }
 
       case REMOVE_FROM_BASKET:
+        const index = state.basket.findIndex((basketItem: any) => basketItem.id === action.id)
+        let newBasket = [...state.basket]
+
+        if(index >= 0) {
+         newBasket.splice(index, 1);
+        } else {
+          console.warn("Can not remove product because is not in the basket");
+        }
         return {
           ...state,
+          basket: newBasket,
           count: state.count - 1,
-          basket: state.basket.filter(((item: any) => item.id !== action.id))
         }
 
       case REMOVE_ALL_ITEMS_FROM_BASKET:
