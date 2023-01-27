526e86b8f758cd4a27a7008d5f0b48138b64d1a4
Peterdinis <peterdinis0@pobox.sk>
2023-01-27 13:00:17 +0100

    refactor: Updating some names

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,8 @@ import AuthContextProvider from "./context/AuthContext";
 import { FallBackLoader } from "./components/shared";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
-import { StateProvider } from "./context/StateContext";
-import stateReducer from "./utils/stateReducer";
+import { StateProvider } from "./context/CardContext";
+import stateReducer from "./utils/cardReducer";
 import { initialState } from "./typings/reduxTypings";
 
 
