c0563e84d74eac8b3447745a62184d270c401623
Peterdinis <peterdinis0@pobox.sk>
2023-01-27 12:54:39 +0100

    feat: Update homepage

--- src/components/hero/Banner.tsx
+++ src/components/hero/Banner.tsx
@@ -1,8 +1,13 @@
 const Banner: React.FC = () => {
   return (
-    <>
-    Fff
-    </>
+    <div className="w-full h-screen bg-opacity-50 bg-black flex justify-center items-center">
+        <div className="mx-4 text-center text-white mb-36">
+          <h1 className="font-bold text-8xl mb-4">FoodReShop</h1>
+          <h2 className="font-bold text-2xl mb-12">
+            Quality food for good prices
+          </h2>
+        </div>
+      </div>
   );
 };
 
