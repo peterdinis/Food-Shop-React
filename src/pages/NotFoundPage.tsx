14106357ec87b5a258d443446cc090ef60554ef4
Peterdinis <peterdinis0@pobox.sk>
2023-01-28 12:06:01 +0100

    fix: Replace a for Link

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,3 +1,5 @@
+import {Link} from "react-router-dom";
+
 const NotFoundPage: React.FC = () => {
   return (
     <>
@@ -13,7 +15,7 @@ const NotFoundPage: React.FC = () => {
             <span className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"></span>
 
             <span className="relative block px-8 py-3 bg-[#1A2238] border border-current">
-              <a href="/">Return to homepage</a>
+              <Link to="/">Return to homepage</Link>
             </span>
           </a>
         </button>
