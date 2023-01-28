634879bfbac2e65ad465162781b1a36c78a31a4c
Peterdinis <peterdinis0@pobox.sk>
2023-01-28 12:38:29 +0100

    style: Formatting code

--- src/components/card/CardSummary.tsx
+++ src/components/card/CardSummary.tsx
@@ -7,7 +7,9 @@ const CardSummary: React.FC = () => {
       <div id="summary" className="w-1/4 px-8 py-10">
         <h1 className="font-semibold text-2xl border-b pb-8">Order Summary</h1>
         <div className="flex justify-between mt-10 mb-5">
-          <span className="font-semibold text-sm uppercase">Items {state.count}</span>
+          <span className="font-semibold text-sm uppercase">
+            Items {state.count}
+          </span>
           <span className="font-semibold text-sm">590$</span>
         </div>
         <div className="border-t mt-8">
@@ -15,9 +17,9 @@ const CardSummary: React.FC = () => {
             <span>Total cost</span>
             <span>$600</span>
           </div>
-            <button className="bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full">
-              Checkout
-            </button>
+          <button className="bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full">
+            Checkout
+          </button>
         </div>
       </div>
     </>
