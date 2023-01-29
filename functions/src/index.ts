c08a36020e952155ef8ee945b755262cdb88be0e
Peterdinis <peterdinis0@pobox.sk>
2023-01-29 12:54:17 +0100

    feat: Updating our backend wip

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,20 @@
 import * as functions from 'firebase-functions';
 import * as express from 'express';
 import * as cors from 'cors';
+import * as stripe from 'stripe';
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express();
+
+app.use(cors({
+  origin: true
+}));
+app.use(express.json());
+
+// testing route
+
 
 // example firebase function
 export const helloWorld = functions.https.onRequest((request, response) => {
