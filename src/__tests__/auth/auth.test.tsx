8890fd22bd85faaabc9a33259608773bd8f93097
Peterdinis <peterdinis0@pobox.sk>
2023-01-29 16:39:46 +0100

    feat: Update auth tests

--- src/__tests__/auth/auth.test.tsx
+++ src/__tests__/auth/auth.test.tsx
@@ -1,14 +1,31 @@
-import { describe, it, expect } from "vitest";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { getErrorMessage } from "../mocks/testErrorMessage";
-import {auth} from "../../firebase/init"
+import { describe, expect, it } from 'vitest';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { getErrorMessage } from '../mocks/testErrorMessage';
+import { auth } from '../../firebase/init';
 
-describe("Firebase authentication tests", () => {
-    it("Try to register user to application", async () => {
+describe('Firebase authentication tests', () => {
+  describe("Firebase register tests", () =>{
+    it('Try to register user to application', async () => {
         try {
-          await signInWithEmailAndPassword(auth, "test@gmail.com", "1234")
+          await signInWithEmailAndPassword(auth, 'test@gmail.com', '1234');
         } catch (err) {
-            getErrorMessage(err)
+          getErrorMessage(err);
         }
-    });
-})
+      });
+    
+      it('Could not register user to app because credentials are wrong', async () => {
+        try {
+          await signInWithEmailAndPassword(auth, 2222, 2222);
+        } catch (err) {
+          getErrorMessage(err);
+        }
+      });
+  })
+
+
+  describe("Firebase login tests", () => {
+    it("Try to login", async() => {
+        return
+    })
+  })
+});
