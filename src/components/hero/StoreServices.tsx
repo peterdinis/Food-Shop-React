b2ab471be5197fd907fb7c84f248eba28745003a
Peterdinis <peterdinis0@pobox.sk>
2023-01-29 16:19:40 +0100

    feat: FOO

--- src/components/hero/StoreServices.tsx
+++ src/components/hero/StoreServices.tsx
@@ -48,6 +48,19 @@ const StoreServices: React.FC = () => {
             <p className="pt-1 text-gray-900">£9.99</p>
           </a>
         </div>
+
+        <div className="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col">
+          <a href="#">
+            <img
+              className="hover:grow hover:shadow-lg"
+              src="https://images.unsplash.com/photo-1555982105-d25af4182e4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80"
+            />
+            <div className="pt-3 flex items-center justify-between">
+              <p className="">Product Name</p>
+            </div>
+            <p className="pt-1 text-gray-900">£9.99</p>
+          </a>
+        </div>
       </div>
     </section>
   );
