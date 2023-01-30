f847a7dd195e604ff7dfe8396164c1860a291634
Peterdinis <peterdinis0@pobox.sk>
2023-01-30 19:19:03 +0100

    feat: Updating card wip

--- src/components/card/EmptyCard.tsx
+++
@@ -1,9 +0,0 @@
-const EmptyCard: React.FC = () => {
-  return (
-    <h5 className="text-center mt-10 font-bold text-3xl">
-      Your card is empty 😌
-    </h5>
-  );
-};
-
-export default EmptyCard;
--- src/components/card/index.ts
+++ src/components/card/index.ts
@@ -2,5 +2,4 @@ import React from 'react';
 
 export const CardWrapper = React.lazy(() => import('./CardWrapper'));
 export const CardInfo = React.lazy(() => import('./CardInfo'));
-export const EmptyCard = React.lazy(() => import('./EmptyCard'));
 export const CardSummary = React.lazy(() => import('./CardSummary'));
