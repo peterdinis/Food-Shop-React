3698af39b30f269a2cca20db98059adb5dbdece1
Peterdinis <peterdinis0@pobox.sk>
2023-01-31 16:33:26 +0100

    feat: Updating products wip

---
+++ src/api/queries/productQueries.ts
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_BACKEND_URL as string
+});
+
+export const getAllProducts = () => api.get("products").then((res) => res.data);
