ada3c1b1aaa2609f2faf65dedec12e5bf466ff05
Peterdinis <peterdinis0@pobox.sk>
2023-02-02 16:34:32 +0100

    feat: New files in api folder

--- src/api/mutations/authMutations.ts
+++ src/api/mutations/authMutations.ts
@@ -1,5 +1,14 @@
 import axios from "axios";
+import { ILoginUser, IRegisterUser } from "../interfaces/IUser";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL as string
 });
+
+export const registerUser = (registerDto: IRegisterUser) => {
+    return api.post("/register", registerDto);
+}
+
+export const loginUser = (loginDto: ILoginUser) => {
+    return api.post("/login", loginDto);
+}
